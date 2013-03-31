6f4760ddd9750aed68b0039959d9db746189f839
Becky Pier <becky@yakketyyak.(none)>
2013-03-31 14:23:13 -0700

    removed deprecated functions

--- euler.py
+++ euler.py
@@ -4,7 +4,6 @@ def make_matrix(read_file):
 	with open(read_file) as input_file:
 		return [[int(i) for i in line.split()] for line in input_file]
 
-
 def products(matrix, length):
 	for i, row in enumerate(matrix):
 		for j, item in enumerate(row):
@@ -19,39 +18,10 @@ def products(matrix, length):
 			except IndexError:
 				pass
 
-def find_greatest_across(greatest, row):
-	for i, item in enumerate(row):
-		if i > (len(row)-4):
-			break
-		product = row[i]*row[i+1]*row[i+2]*row[i+3]
-		#print "%d * %d * %d * %d = %d." % (row[i], row[i+1], row[i+2], row[i+3], product)
-		if product > greatest:
-			print "%d * %d * %d * %d = %d." % (row[i], row[i+1], row[i+2], row[i+3], product)
-			print "The new greatest is: ", product
-			greatest = product
-	print row
-	print greatest
-	return greatest
-
-def find_greatest_down(matrix):
-	new_matrix = []
-	new_row = []
-	for i, row in enumerate(matrix):
-		new_row.append(row[i])
-		
-
-def find_greatest_diagonal(greatest, matrix):
-	pass
-
 def main(argv):
 	args = sys.argv
 	script, read_file = argv
 	length = 4
 	print max(products(make_matrix(read_file), length))
-	greatest = 0
-
-	#find_greatest_down(matrix)
-	#greatest = find_greatest_across(greatest, row)
-	#print greatest
 
 main(sys.argv)
